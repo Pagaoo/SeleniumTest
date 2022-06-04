0103af09ef9754ec43659b53318a20da1e5da741
Pagão <gabrielpaganinibarroso@hotmail.com>
2022-06-03 21:34:10 -0300

    Later in the year i'll try to config a slow scroll through the page

--- src/test/java/com/example/trabalhotestes/MainTest.java
+++ src/test/java/com/example/trabalhotestes/MainTest.java
@@ -1,14 +1,10 @@
 package com.example.trabalhotestes;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.sql.Time;
@@ -36,6 +32,7 @@ public class MainTest {
     @Test
     void openGoogle() {
         driver.get("https://m.manganelo.com/www");
+        driver.manage().window().maximize();
 
         searchManga();
 
@@ -46,14 +43,26 @@ public class MainTest {
 
     public void searchManga() {
         WebElement searchManga = driver.findElement(By.id("search-story"));
-        searchManga.sendKeys("Naruto");
+        searchManga.sendKeys("Modern Romanesco");
     }
 
     public void navigateIntoManga() {
-        WebElement clickManga = driver.findElement(By.xpath("//*[@id=\"1332\"]"));
+        WebElement clickManga = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/form/div/ul/a/li"));
         clickManga.click();
 
-        WebElement clickChapter = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[3]/ul/li[748]/a"));
+        int numElments = countChapters();
+
+        WebElement clickChapter = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[3]/ul/li[last()]"));
         clickChapter.click();
+
+        for(int i = 0; i < numElments - 1; i++) {
+            WebElement clickNextChapter = driver.findElement(By.xpath("/html/body/div[1]/div[6]/div[1]/div/a[last()]"));
+            clickNextChapter.click();
+        }
+    }
+
+    public int countChapters() {
+        int countElement = driver.findElements(By.tagName("li")).size();
+        return countElement;
     }
 }
